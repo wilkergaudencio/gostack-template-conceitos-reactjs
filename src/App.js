1135e34b946bdf3a0efd6092c5328e10c5b303a4
Wilker Gaudencio <wilker@gaudit.com.br>
2020-04-12 13:43:01 -0300

    react list and remove projects

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState, useEffect }  from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories')
+        .then(response => {
+          console.log(response)
+          setRepositories(response.data)
+        })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    await api.post('repositories', {
+      title: "Novo Desafio",
+      url: "teste",
+      techs: ["ReactJS"]
+    }).then(response => {
+      setRepositories([...repositories, response.data]);
+    })
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+     await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter(repo => repo.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+      {repositories.map(repository => (
+          <li key={repository.id}>
+          <p>{repository.title}</p>
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
